ce4344e7792953e7fb858685eaabbae099da5e11
Carlos C <uldericofilho@gmail.com>
2016-01-30 02:15:34 +0100

    Refactor to improve performance of Analyze call

--- checker.go
+++ checker.go
@@ -13,6 +13,31 @@ const (
 	negative sign = -1
 )
 
+// analysis is the complete sentiment calculation
+type analysis struct {
+	// positivityScore is the sum of the positive sentiment points of the
+	// analyzed text.
+	positivityScore float64
+
+	// negativityScore is the sum of the negativity sentiment points of the
+	// analyzed text.
+	negativityScore float64
+
+	// positivityComparative establishes a ratio of sentiment per positive
+	// word
+	positivityComparative float64
+
+	// negativityComparative establishes a ratio of sentiment per negative
+	// word
+	negativityComparative float64
+
+	// positiveWords is the list of positive words for a given sentiment.
+	positiveWords []string
+
+	// negativeWords is the list of negative words for a given sentiment.
+	negativeWords []string
+}
+
 // Score is the result of sentiment calculation
 type Score struct {
 	// Score is the sum of the sentiment points of the analyzed text.
@@ -22,7 +47,7 @@ type Score struct {
 	// Comparative establishes a ratio of sentiment per word
 	Comparative float64
 
-	// List of words for a given sentiment.
+	// Words is the list of words for a given sentiment.
 	Words []string
 }
 
@@ -45,9 +70,9 @@ type FullScore struct {
 
 var lettersAndSpaceOnly = regexp.MustCompile(`[^a-zA-Z ]+`)
 
-func calculateScore(phrase string, calcSign sign) Score {
-	var hits float64
-	var words []string
+func calculateScore(phrase string) analysis {
+	var phits, nhits float64
+	var pwords, nwords []string
 	var count int
 
 	scanner := bufio.NewScanner(strings.NewReader(strings.ToLower(lettersAndSpaceOnly.ReplaceAllString(phrase, " "))))
@@ -56,39 +81,60 @@ func calculateScore(phrase string, calcSign sign) Score {
 		count++
 		word := scanner.Text()
 		if v, ok := afinn[word]; ok {
-			if (calcSign == positive && v > 0) || (calcSign == negative && v < 0) {
-				hits += v * float64(calcSign)
-				words = append(words, word)
+			if v > 0 {
+				phits += v
+				pwords = append(pwords, word)
+			} else if v < 0 {
+				nhits -= v
+				nwords = append(nwords, word)
 			}
 		}
 	}
 
+	return analysis{
+		positivityScore:       phits,
+		positivityComparative: phits / float64(count),
+		positiveWords:         pwords,
+		negativityScore:       nhits,
+		negativityComparative: nhits / float64(count),
+		negativeWords:         nwords,
+	}
+}
+
+func renderNegativeScore(a analysis) Score {
 	return Score{
-		Score:       hits,
-		Comparative: hits / float64(count),
-		Words:       words,
+		Score:       a.negativityScore,
+		Comparative: a.negativityComparative,
+		Words:       a.negativeWords,
 	}
 }
 
 // Negativity calculates the negative sentiment of a sentence
 func Negativity(phrase string) Score {
-	return calculateScore(phrase, negative)
+	return renderNegativeScore(calculateScore(phrase))
+}
+
+func renderPositiveScore(a analysis) Score {
+	return Score{
+		Score:       a.positivityScore,
+		Comparative: a.positivityComparative,
+		Words:       a.positiveWords,
+	}
 }
 
 // Positivity calculates the positive sentiment of a sentence
 func Positivity(phrase string) Score {
-	return calculateScore(phrase, positive)
+	return renderPositiveScore(calculateScore(phrase))
 }
 
 // Analyze calculates overall sentiment
 func Analyze(phrase string) FullScore {
-	pos := Positivity(phrase)
-	neg := Negativity(phrase)
+	analysis := calculateScore(phrase)
 
 	return FullScore{
-		Score:       pos.Score - neg.Score,
-		Comparative: pos.Comparative - neg.Comparative,
-		Positive:    pos,
-		Negative:    neg,
+		Score:       analysis.positivityScore - analysis.negativityScore,
+		Comparative: analysis.positivityComparative - analysis.negativityComparative,
+		Positive:    renderPositiveScore(analysis),
+		Negative:    renderNegativeScore(analysis),
 	}
 }
